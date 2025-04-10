492bc7db392b3a42febbdf69211e103da3aaefd5
parkjooyoung <mirkduddl@naver.com>
2025-04-11 02:09:41 +0900

    ci: Github Actions를 이용해 master 브랜치 push 시 Storybook 자동 배포 설정 추가

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,19 +1,20 @@
-import type { StorybookConfig } from '@storybook/react-vite';
+import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
-  "stories": [
-    "../src/**/*.mdx",
-    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test"
+    "@storybook/experimental-addon-test",
   ],
-  "framework": {
-    "name": "@storybook/react-vite",
-    "options": {}
-  }
+  framework: {
+    name: "@storybook/react-vite",
+    options: {},
+  },
+  viteFinal: async (config) => {
+    config.base = "/react-toast-notification/";
+    return config;
+  },
 };
 export default config;
